391b638f5d71fee3bcdba6de94774a3faa0135ef
mdshahadad <identity.shahadad@gmail.com>
2024-11-23 00:57:25 +0600

    Added Task 3: Object Types, Type Alias, & Literal Types

--- module2/src/practice.ts
+++ module2/src/practice.ts
@@ -14,7 +14,34 @@
     }
 
     const user1 = user('shahadad', 122, 'admin');
-    console.log(user1)
+    console.log(user1);
+
+    // Task 3: Object Types, Type Alias, & Literal Types
+    type Person = {
+        name: string;
+        address: string;
+        hairAndEyeColor: string;
+        incomeAndExpense: [number, number];
+        hobbies: string;
+        familyMembers: number;
+        job: string;
+        skills: string[];
+        maritalStatus: string;
+        friends: string[];
+    };
+
+    const person1: Person = {
+        name: 'Shahadad',
+        address: 'Wasa Dampara',
+        hairAndEyeColor: 'Black',
+        incomeAndExpense: [10000, 5000],
+        hobbies: 'Cricket',
+        familyMembers: 4,
+        job: 'None',
+        skills: ['Javascript', 'Reactjs', 'Nodejs', 'Expressjs'],
+        maritalStatus: 'Unmarried',
+        friends: ['Muntasir', 'Avijit', 'Abdur', 'Ifti'],
+    }
 
 
 }
